7c95ae393b0b175dd2b9058a5e248d173346d148
RayhanHakim <rayhanhakim@DESKTOP-8DQG0RP.localdomain>
2020-05-23 22:33:49 +0700

    message

---
+++ Donasi-Bersama/app/controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo '<h1>Hello</h1>';
+        exit;
+    }
+
+}
+
